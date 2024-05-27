e6b5b946773e17dca0f1118b16b75e4d348d83a8
peterson-e <55142305+peterston-e@users.noreply.github.com>
2024-05-27 15:34:33 +0100

    saving point2

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,19 @@
 "use client";
+
+import { EnvelopeOpenIcon } from "@radix-ui/react-icons";
+import { CommandDemo } from "./Command";
+import { Button } from "@/components/ui/button";
+import { BellIcon } from "lucide-react";
+
 export default function Header() {
 	return (
-		<div className="flex gap-2 border-r min-h-screen p-4">hello header</div>
+		<div className="grid grid-cols-2 gap-4 border-b p-4">
+			<CommandDemo />
+			<div className="flex items-center justify-end">
+				<Button variant="outline" size="icon">
+					<BellIcon className=" h-4 w-4" />
+				</Button>
+			</div>
+		</div>
 	);
 }
